9adddb352b1728129f687c21d7b12a712e59cf09
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-09-30 22:05:30 -0300

    feat: remove a debug console.log

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -7,7 +7,6 @@ export const GET = async (request, { params }) => {
         await connectToDb();
         const user = await User.findById(userId);
         if (!user) return new Response("User not found", { status: 404 });
-        console.log(user);
         return new Response(JSON.stringify(user), { status: 200 });
     } catch (error) {
         console.error(error);
